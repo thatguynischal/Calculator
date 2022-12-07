584c85bae74718ccbcb2aea5a7967c5e3f048c5f
Nischal Khadka <justchalchal@gmail.com>
2022-12-07 18:13:47 +0545

    Final Commit

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { evaluate } from "mathjs";
-import * as math from "mathjs"
+import * as math from "mathjs";
 
 const Calculator = () => {
-  const btnColor = "#FF9500";
+  const btnColor = "#562B08";
   const [text, setText] = useState("");
   const [result, setResult] = useState("");
 
@@ -19,48 +19,58 @@ const Calculator = () => {
 
   const calculateResult = () => {
     const input = text.join(""); //removes commas
-    setResult(math.evaluate(input))
+    setResult(math.evaluate(input));
+  };
+
+  const backSpace = () => {
+    const pop = text.pop();
+    setText((text) => [...text]);
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center gap-2">
-      <Input text={text} result={result} />
-      <div className="flex gap-2">
-        <Buttons symbol="7" handleClick={addToText}/>
-        <Buttons symbol="8" handleClick={addToText}/>
-        <Buttons symbol="9" handleClick={addToText}/>
-        <Buttons symbol="/" color={btnColor} handleClick={addToText}/>
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-2 bg-primary">
+      <div className="p-7 rounded-lg border">
+        <Input text={text} result={result} />
+        <div className="flex gap-2">
+          <Buttons symbol="7" handleClick={addToText} />
+          <Buttons symbol="8" handleClick={addToText} />
+          <Buttons symbol="9" handleClick={addToText} />
+          <Buttons symbol="/" color={btnColor} handleClick={addToText} />
+        </div>
+        <div className="flex gap-2">
+          <Buttons symbol="4" handleClick={addToText} />
+          <Buttons symbol="5" handleClick={addToText} />
+          <Buttons symbol="6" handleClick={addToText} />
+          <Buttons symbol="*" color={btnColor} handleClick={addToText} />
+        </div>
+        <div className="flex gap-2">
+          <Buttons symbol="1" handleClick={addToText} />
+          <Buttons symbol="2" handleClick={addToText} />
+          <Buttons symbol="3" handleClick={addToText} />
+          <Buttons symbol="+" color={btnColor} handleClick={addToText} />
+        </div>
+        <div className="flex gap-2">
+          <Buttons symbol="0" handleClick={addToText} />
+          <Buttons symbol="." handleClick={addToText} />
+          <Buttons symbol="=" handleClick={calculateResult} />
+          <Buttons symbol="-" color={btnColor} handleClick={addToText} />
+        </div>
+        <div className="flex gap-2">
+          <Buttons symbol="Clear" handleClick={clear} width="105px" />
+          <Buttons symbol="DEL" handleClick={backSpace} width="105px" />
+        </div>
       </div>
-      <div className="flex gap-2">
-        <Buttons symbol="4" handleClick={addToText}/>
-        <Buttons symbol="5" handleClick={addToText}/>
-        <Buttons symbol="6" handleClick={addToText}/>
-        <Buttons symbol="*" color={btnColor} handleClick={addToText}/>
-      </div>
-      <div className="flex gap-2">
-        <Buttons symbol="1" handleClick={addToText}/>
-        <Buttons symbol="2" handleClick={addToText}/>
-        <Buttons symbol="3" handleClick={addToText}/>
-        <Buttons symbol="+" color={btnColor} handleClick={addToText}/>
-      </div>
-      <div className="flex gap-2">
-        <Buttons symbol="0" handleClick={addToText}/>
-        <Buttons symbol="." handleClick={addToText}/>
-        <Buttons symbol="=" handleClick={calculateResult}/>
-        <Buttons symbol="-" color={btnColor} handleClick={addToText}/>
-      </div>
-      <Buttons symbol="Clear" handleClick={clear}/>
     </div>
   );
 };
 
-const Buttons = ({ symbol = "", color, handleClick }) => {
+const Buttons = ({ symbol = "", color, handleClick, width }) => {
   return (
     <div>
       <button
         onClick={() => handleClick(symbol)}
-        className="btn"
-        style={{ backgroundColor: color }}
+        className="myBtn"
+        style={{ backgroundColor: color, width: width }}
       >
         {symbol}
       </button>
@@ -70,7 +80,7 @@ const Buttons = ({ symbol = "", color, handleClick }) => {
 
 const Input = ({ text, result }) => {
   return (
-    <div className="h-24 w-52 flex justify-end items-end flex-col bg-white text-black p-4 rounded">
+    <div className="h-24 flex justify-end items-end flex-col bg-white text-black p-4 rounded">
       <div className="result flex-1 text-2xl">{text}</div>
       <div className="text flex-1 text-xl">{result}</div>
     </div>
